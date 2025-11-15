e0b4eeaf6e1ee6e15a7bd75c69af41beeaaf449e
avAs <abas.zade@outlook.com>
2025-11-15 13:47:11 +0330

    add new package jalali converter

---
+++ packages/jalali_convertor.py
@@ -0,0 +1,20 @@
+from typing import Any
+from datatime import datetime
+from persiantools.jdatetime import JalaliDate
+
+
+class Jalali:
+    def __call__(self):
+        return self.converter(date, time)
+    
+    def __str__(self) -> str:
+        pass
+    
+    def __repr__(self) -> str:
+        pass
+    
+    def converter(self, date, time):
+        
+    
+    
+    
