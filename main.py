dc265a88bb1093e2aebf1e423f428e419aa6e2b4
avas <amirpc36@gmail.com>
2025-11-15 23:31:11 +0330

    alaki

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from packages.excel_manager import ExcelManager
 from packages.ploter import PlotManager
 from packages.safe_round import NumberRounder
 
@@ -8,17 +9,23 @@ from packages.safe_round import NumberRounder
 # print(rounder.round_list([1.25153, 2.355158, 0, None], zero_return=False))
 
 
-x_list = [1, 2, 3, 4, 5]
-y_list = [1, 5, 14, 25, 40]
+# x_list = [1, 2, 3, 4, 5]
+# y_list = [1, 5, 14, 25, 40]
 
-plot = PlotManager()
+# plot = PlotManager()
 
-plot.line_plot(
-    x_data=x_list,
-    y_data=y_list,
-    title="avAs",
-    xlabel="amir",
-    ylabel="zade",
-    label="abaszade",
-    grid=False,
-)
+# plot.line_plot(
+#     x_data=x_list,
+#     y_data=y_list,
+#     title="avAs",
+#     xlabel="amir",
+#     ylabel="zade",
+#     label="abaszade",
+#     grid=False,
+# )
+
+
+excel = ExcelManager("1404.xlsx")
+excel.load_workbook()
+excel.set_active_sheet("1404")
+print(excel.read_range(start_cell="A1", end_cell="D10"))
