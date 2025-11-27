937f3412edfddde811e5ec0f3de4bf8d0b7728d8
avas <amirpc36@gmail.com>
2025-11-27 21:16:18 +0330

    find a way to start work with eitta

--- packages/selenium_eitta.py
+++ packages/selenium_eitta.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import by
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-driver = webdriver.chrome()
+driver = webdriver.Chrome()
 
 driver.get("https://web.eitaa.com/")
+
+
+element = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located(
+        (
+            By.CSS_SELECTOR,
+            "html > body > div:first-of-type > div > div:nth-of-type(2) > div:first-of-type > div > h4",
+        )
+    )
+)
+
+if element:
+    print("are amoo josamesh")
+else:
+    print("element not found")
+
+input("press any key to close")
